0d5bc13d248bb7ba9b6d389c4648df4480eb23ff
Kévøø tech <167717269+kevoomedia@users.noreply.github.com>
2024-05-23 20:22:50 +0300

    këvøø.js

--- commandes/bmw.js
+++ commandes/bmw.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "bmw", reaction: "😜", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "këvøø", reaction: "😜", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝒀𝒆𝒔😉 𝑨𝒎 𝑩𝑴𝑾 𝑴𝑫 𝑨 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔\n\n ' + "𝑰𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒖𝒔𝒆 𝒎𝒆 𝒋𝒖𝒔𝒕 𝒕𝒚𝒑𝒆 𝒎𝒆𝒏𝒖 𝒕𝒐 𝒄𝒐𝒎𝒎𝒂𝒏𝒅 𝒎𝒆";
+    let z = '𝒀𝒆𝒔😜 𝑨𝒎 𝑩𝑴𝑾 𝑴𝑫 𝑨 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔\n\n ' + "𝑰𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒖𝒔𝒆 𝒎𝒆 𝒋𝒖𝒔𝒕 𝒕𝒚𝒑𝒆 𝒎𝒆𝒏𝒖 𝒕𝒐 𝒄𝒐𝒎𝒎𝒂𝒏𝒅 𝒎𝒆";
     let d = '                                                🏁𝑨𝒎 𝒕𝒉𝒆 𝒇𝒂𝒔𝒕𝒆𝒔𝒕 𝒃𝒐𝒕 𝒊𝒏 𝒕𝒉𝒆 𝒘𝒐𝒓𝒍𝒅';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/d98532bb4aa4569e92afb.mp4';
@@ -20,8 +20,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Kévøø😜-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Kévøø😜-MD-REPO*'
+   let z ='Salut je m\'appelle *Këvøø😜-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Këvøøø😜-MD-REPO*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
